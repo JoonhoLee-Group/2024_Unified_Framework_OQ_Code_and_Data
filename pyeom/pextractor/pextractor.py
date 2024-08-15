74f32b8532fc50d8cfb22c8d6ce79e6dcbe06117
felixivander <50121236+felixivander@users.noreply.github.com>
2024-08-15 02:29:02 -0400

    Delete pyeom directory

--- pyeom/pextractor/pextractor.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def pextractor_exact(rho):
-    A0inv = np.linalg.inv(rho[:,:,0])
-    U=np.zeros(len(rho[:,0,0]),len(rho[:,0,0]),len(rho[0,0,:]))
-    for i in range(1,len(rho[0,0,:])): #this fits the propagators from time series data
-        U[:,:,i] = (np.matmul(rho[:,:,i],A0inv))
-    return U
